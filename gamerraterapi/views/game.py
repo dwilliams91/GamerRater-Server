d5f4801af237875265235dc3600d2ce4b72a40a3
Your Github User Name <david.lav.williams@gmail.com>
2021-02-12 13:30:55 -0600

    the delete games works

--- gamerraterapi/views/game.py
+++ gamerraterapi/views/game.py
@@ -23,6 +23,39 @@ class All_Games(ViewSet):
         serializer=GameSerializer(games, many=True, context={'request':request})
         return Response(serializer.data)
 
+    def create(self, request):
+        gamer=Gamers.objects.get(user=request.auth.user)
+        game=Games()
+        game.title=request.data['title']
+        game.description=request.data['description']
+        game.year=request.data['year']
+        game.number_of_players=request.data['numberOfPlayers']
+        game.play_time=request.data['playTime']
+        game.age_recommendation=request.data['ageRecommendation']
+        game.gamer= gamer
+
+
+        try:
+            game.save()
+            serializer=GameSerializer(game, context={'request': request})
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        print("hello")
+        try:
+            game=Games.objects.get(pk=pk)
+            game.delete()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        
+        except Game.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model= Games
